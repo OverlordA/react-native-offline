53d1dd2e9bff6a35cd81a3af0e8d644966ff1048
anton <antonkotik0@gmail.com>
2019-11-18 17:03:40 +0200

    - configure build bet release and debug

---
+++ assets/images/index.js
@@ -0,0 +1,14 @@
+const images = {
+  appWelcome1: require('./app_welcome_image_1.png'),
+  appWelcome2: require('./app_welcome_image_2.png'),
+  appWelcome3: require('./app_welcome_image_3.png'),
+  finger: require('./finger.png'),
+  money: require('./money.png'),
+  priceTag: require('./priceTag.png'),
+  hamburger: require('./hamburger.png'),
+  person: require('./person.png'),
+  questionmark: require('./questionmark.png'),
+  notepad: require('./notepad.png'),
+};
+
+export default images;
